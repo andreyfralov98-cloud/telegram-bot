17ccdef64b6fbef273abf6fa5f33471025c4d2c7
andreyfralov98-cloud <andrey.fralov.98@bk.ru>
2026-03-09 10:54:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,9 @@ def control_menu():
 # === ЗАХВАТ ПОСТОВ ===
 @dp.channel_post_handler(chat_id=SOURCE_CHANNEL_ID, content_types=types.ContentType.ANY)
 async def grab_post(message: types.Message):
+    if queue:
+    publish_at = queue[-1]["publish_at"] + DELAY_SECONDS
+    else:
     publish_at = time.time() + DELAY_SECONDS
 
     if message.text:
@@ -195,6 +198,7 @@ if __name__ == "__main__":
     print("🚀 Бот запускается")
     load_queue()
     executor.start_polling(dp, on_startup=on_startup)
+
 
 
 
