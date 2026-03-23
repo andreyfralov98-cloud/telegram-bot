82d90b47ab270fcffdba7c9b37f05f6bb7974eca
andreyfralov98-cloud <andrey.fralov.98@bk.ru>
2026-03-23 09:45:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -125,19 +125,25 @@ async def grab_post(message: types.Message):
 
     n = now_local()
 
-    if queue:
-        publish_at = queue[-1]["publish_at"] + DELAY_SECONDS
+if queue:
+    last_time = queue[-1]["publish_at"]
+
+    if last_time < time.time():
+        publish_at = time.time() + DELAY_SECONDS
     else:
-        if n.hour < PUBLISH_START_HOUR:
-            target = n.replace(hour=6, minute=0, second=0, microsecond=0)
-            publish_at = target.timestamp()
+        publish_at = last_time + DELAY_SECONDS
 
-        elif n.hour >= PUBLISH_END_HOUR:
-            publish_at = next_day_6am_ts()
+else:
+    if n.hour < 6:
+        target = n.replace(hour=6, minute=0, second=0, microsecond=0)
+        publish_at = target.timestamp()
 
-        else:
-            publish_at = time.time() + DELAY_SECONDS
+    elif n.hour >= 24:
+        publish_at = next_day_6am_ts()
 
+    else:
+        publish_at = time.time() + DELAY_SECONDS
+        
     # --- ТЕКСТ ---
     if message.text:
         queue.append({
